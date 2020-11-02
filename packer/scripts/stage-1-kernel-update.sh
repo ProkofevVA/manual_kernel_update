41ce8a4c265068bfceb85bc00dcebbf56b08910e
Владимир Прокофьев <vprokofev@p-rbta-msk-lt60c1.astralinux.ru>
2020-11-02 05:41:58 +0300

    centos 7 with kernel 5

--- packer/scripts/stage-1-kernel-update.sh
+++ packer/scripts/stage-1-kernel-update.sh
@@ -2,8 +2,8 @@
 
 # Install elrepo
 yum install -y http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
-# Install new kernel
-yum --enablerepo elrepo-kernel install kernel-ml -y
+# Install new kernel, headers and devel for future install of VBoxGuesAdditions
+yum --enablerepo elrepo-kernel install kernel-ml kernel-mk-headers kernel-nl-devel -y
 # Remove older kernels (Only for demo! Not Production!)
 rm -f /boot/*3.10*
 # Update GRUB
@@ -12,3 +12,5 @@ grub2-set-default 0
 echo "Grub update done."
 # Reboot VM
 shutdown -r now
+
+
